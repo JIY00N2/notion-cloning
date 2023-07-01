f1b171b0ea9df94d7ddf7d7890da08c68d5da449
JiYoon <angella990825@gmail.com>
2023-07-01 14:10:22 +0900

    refactor: postlist.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,29 +1,67 @@
 import { push } from '../domain/router.js';
-export default function PostList({ $target, initialState, onPostClick }) {
+export default function PostList({ $target, initialState, onAdd, onDelete }) {
   const $postList = document.createElement('div');
   $target.appendChild($postList);
 
   // 값 체크 필수
   this.state = initialState;
+
   // nextState로 업데이트
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
   };
 
-  this.render = () => {
-    $postList.innerHTML = `
+  const drawDocumentList = (list) => {
+    const text = `
       <ul>
-        ${this.state.map((post) => `<li data-id=${post.id}>${post.title}</li>`).join('')}
+      ${list
+        .map(
+          ({ id, title, documents }) => `
+      <div class='documentsTree'>
+        <li data-id="${id}">
+        ${title}
+        <button class="add-button"> + </button>
+        <button class="delete-button"> - </button>
+        </li>
+        ${documents.map((document) => drawDocumentList([document])).join('')}
+      </div>
+      `,
+        )
+        .join('')}
       </ul>
+      `;
+
+    return text;
+  };
+
+  this.render = () => {
+    const documentsList = drawDocumentList(this.state);
+    const documentAddButton = `<button class="add-button">➕ 페이지 추가하기</button>`;
+    $postList.innerHTML = `
+      <div class="list">${documentsList}${documentAddButton}</div>
     `;
   };
+
   this.render();
+
   $postList.addEventListener('click', (e) => {
     const $li = e.target.closest('li');
+    const { className } = e.target;
+    const id = $li?.dataset.id;
+    if (className) {
+      if (className === 'delete-button') {
+        onDelete(id);
+        return;
+      } else {
+        onAdd(id, className);
+        return;
+      }
+    }
     if ($li) {
-      const { id } = $li.dataset;
-      push(`/posts/${id}`);
+      push(`/documents/${id}`);
     }
   });
 }
+
+// ?. 객체 체인에서 속성에 안전하게 접근하고, 코드의 간결성과 에러 방지에 도움
