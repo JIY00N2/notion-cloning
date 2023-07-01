e26b2c144c8c847f6b356ff3356fa09261557a87
JiYoon <angella990825@gmail.com>
2023-07-01 14:09:50 +0900

    feat: sidebar.js

---
+++ src/components/SidebarList.js
@@ -0,0 +1,47 @@
+import { push } from '../domain/router';
+
+export default function SidebarList({ $target, initialState }) {
+  // new 검사
+  const $sidebarList = document.createElement('div');
+  $target.appendChild($sidebarList);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = [nextState];
+    this.render();
+  };
+
+  const sidebarList = (list) => {
+    const text = `
+    <ul>
+      ${list
+        .map(
+          ({ id, title, documents }) => `
+        <div class="documents-tree">
+          <li data-id="${id}">${title}</li>
+          ${documents.map((document) => sidebarList([document])).join('')}
+        </div>
+      `,
+        )
+        .join('')}
+    </ul>
+    `;
+    return text;
+  };
+
+  this.render = () => {
+    const documentsList = sidebarList(this.state);
+    $sidebarList.innerHTML = `<div>${documentsList}</div>`;
+  };
+
+  this.render();
+
+  $sidebarList.addEventListener('click', (e) => {
+    const $li = e.target.closest('li');
+    const id = $li.dataset.id;
+    if ($li) {
+      push(`/documents/${id}`);
+    }
+  });
+}
