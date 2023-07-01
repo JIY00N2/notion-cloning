c013b68cd6d3d2893823b0f7e29c19fa843d185f
JiYoon <angella990825@gmail.com>
2023-07-01 14:08:28 +0900

    feat: Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,37 +6,27 @@ export default function Editor({
   },
   onEditing,
 }) {
-  const $editor = document.createElement('div');
+  // new.target 검사
 
-  $editor.innerHTML = `
-      <input type='text' name='title' style ='width:600px;'/>
-      <div name='content' contentEditable="true" style='width:600px; height: 400px border: 1px solid black; padding: 8px;'></div>
-    `;
+  const $editor = document.createElement('div');
+  $target.appendChild($editor);
 
   this.state = initialState;
-  $target.appendChild($editor);
 
   this.setState = (nextState) => {
     this.state = nextState;
-    this.render();
+    const { title, content } = this.state;
+    $editor.querySelector('[name=title]').value = title;
+    $editor.querySelector('[name=content]').value = content;
   };
 
   this.render = () => {
-    const richContent = this.state.content
-      .split('\n')
-      .map((line) => {
-        if (line.indexOf('# ') === 0) {
-          return `<h1>${line.substr(2)}</h1>`;
-        } else if (line.indexOf('## ') === 0) {
-          return `<h2>${line.substr(3)}</h2>`;
-        } else if (line.indexOf('### ') === 0) {
-          return `<h3>${line.substr(4)}</h3>`;
-        }
-        return line;
-      })
-      .join('<br>');
-    $editor.querySelector('[name=title]').value = this.state.title;
-    $editor.querySelector('[name=content]').textContent = richContent;
+    const { title, content } = this.state;
+    $editor.innerHTML = `
+      <input type='text' name='title' value="${title}"/>
+      <textarea name="content">${content}</textarea>
+    `;
+
     // 개행처리가 안되는 이유?
     // 서버에서 내려오는 개행 값은 \n
     // textarea에서는 \n으로 개행을 처리해주는데 innerHTML은 replace를 이용해서 처리
@@ -53,8 +43,9 @@ export default function Editor({
   $editor.querySelector('[name=content]').addEventListener('input', (e) => {
     const nextState = {
       ...this.state,
-      content: e.target.textContent,
+      content: e.target.value,
     };
     this.setState(nextState);
+    onEditing(this.state);
   });
 }
